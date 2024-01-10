07650611ce683bf46af23f293f5d5c755074608e
SolomonChidera <solochidera10@gmail.com>
2024-01-10 11:00:26 +0100

    [#10]First Implementation of the console Handled the help command Handled the EOF command Default prompt ==> (hbnb)

--- console.py
+++ console.py
@@ -1,17 +1,25 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""
+   A generic class to build line-oriented command interpreters.
+   Interpreters constructed with this class obey the following conventions:
+   cmd module where the console will be built with
+"""
+
 import cmd
 
 
 class HBNBCommand(cmd.Cmd):
+    """Main class of the console"""
     prompt = '(hbnb) '
-    
+
     def do_quit(self, arg):
         """Quit command to exit the program"""
         return True
-    
+
     def do_EOF(self, arg):
         """Exiting the program on EOF"""
         return True
-    
+
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
