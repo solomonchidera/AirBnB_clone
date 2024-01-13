478b08850a9b17fc0da55388738f64d3ebae3ab2
Moniaar <www.omniaahmed1562001@gmail.com>
2024-01-13 06:34:52 -0800

    #18 This class was modifed for the 10th time

--- models/base_model.py
+++ models/base_model.py
@@ -1,52 +1,45 @@
 #!/usr/bin/python3
 """This is the base class for everything in this module"""
+
+
 import uuid
 from datetime import datetime
 import models
-# Import the storage variable
+
 
 class BaseModel:
-    """This is the grandfather for all of the upcoming classes"""
-    def __init__(self, *args, **kwargs):
-        """The constructor"""
-        if kwargs:
-            for key, date_value in kwargs.items():
-                if key == "created_at":
-                    date_time = '%Y-%m-%dT%H:%M:%S.%f'
-                    date_value = datetime.strptime(date_value, date_time)
-
-                    if key == "updated_at":
-                        date_value = datetime.strptime(date_value, date_time)
-                        if key != "__class__":
-                            setattr(self, key, date_value)
-
-        else:
-            self.id = str(uuid.uuid4())
-            # save new created at
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
-
-    def __str__(self):
-        """Printing the objects str method"""
-
-        new_class = self.__class__.__name__
-        return "[{}]({}){}".format(new_class, self.id, self.__dict__)
+    """This is the base model that will identity everything"""
+    def __init__(self):
+        """This is the init method for every instance created"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def save(self):
-        """updates the public instance attribute updated_at
-        with the current datetime"""
+        """ Save the instance to the JSON file """
+        objects[self.__class__.__name__ + "." + self.id] = self
+        storage.save()
 
-        self.updated_at = datetime.now()
-        # saving mechanism using JSON
-        models.storage.save()
+    def __str__(self):
+        """ String representation of the instance """
+        return f"""[{self.__class__.__name__}]({self.id})
+        {{'created_at': {self.created_at}, 'id': '{self.id}',
+            'updated_at': {self.updated_at}}}"""
+        return f"""[{self.__class__.__name__}]({self.id})
+        {{'created_at': datetime.datetime({self.created_at}),
+            'id': '{self.id}',
+            'updated_at': datetime.datetime({self.updated_at})}}"""
 
-    def to_dict(self):
 
-        """returns a dictionary containing all keys/values of
-        __dict__ of the instance"""
-        dic_copy = self.__dict__.copy()
-        dic_copy["created_at"] = self.created_at.isoformat()
-        dic_copy["updated_at"] = self.updated_at.isoformat()
-        dic_copy['__class__'] = self.__class__.__name__
-        return dic_copy
+class Storage:
+    """This is tthe main sotrage file that will store everything"""
+    def save(self):
+        """ Save instances to a JSON file (dummy implementation) """
+        with open("data.json", "w") as file:
+            json.dump(
+                    {key: str(value) for key, value in objects.items()}, file)
+
+
+objects = {}
+classes = {"BaseModel": BaseModel}
+storage = Storage()
